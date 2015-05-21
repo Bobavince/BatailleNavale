1e84a0e5e6b76732055ce84d8447039d07229c30
Bobavince <vince.boba@gmail.com>
2015-05-21 08:13:26 +0200

    Modification utile du premier fichier

--- TestJeu.java
+++ TestJeu.java
@@ -3,7 +3,10 @@
  * Classe de test lors de la phase de conception.
  * NE SERA PAS DANS LA VERSION FINALE DU JEU !
  */
+// Les imports nécessaires
+import javax.swing.JFrame; // Pour l'affichage en fenetre si on veut évoluer
 
+		
 public class TestJeu {
 
 	/**
@@ -11,7 +14,20 @@ public class TestJeu {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		 	//MORCEAU DE CODE POUR ESSAYER L'AFFICHAGE EN FENETRE. A FAIR EN DERNIER
+		    JFrame fenetre = new JFrame();
+		                
+		    //Définit un titre pour notre fenêtre
+		    fenetre.setTitle("BatailleNAVALE");
+		    //Définit sa taille : 400 pixels de large et 100 pixels de haut
+		    fenetre.setSize(800, 600);
+		    //Nous demandons maintenant à notre objet de se positionner au centre
+		    fenetre.setLocationRelativeTo(null);
+		    //Termine le processus lorsqu'on clique sur la croix rouge
+		    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    //Et enfin, la rendre visible        
+		    fenetre.setVisible(true);
 	}
 
 }
