933ca67c1ce8edb6dfd34b8f5c04d8b12a99d91b
Bobavince <vince.boba@gmail.com>
2015-05-22 23:43:52 +0200

    Quelques modfis encore

--- Bateau.java
+++ Bateau.java
@@ -7,12 +7,26 @@
 
 public class Bateau {
 	int numero;
+	boolean alive = false;
 	int longueur;
 	int largeur;
 	
 	public Bateau(int longueur, int largeur){
 		
 	}
+	
+	public void creerBateau(int numero){
+		this.numero = numero;
+	}
+	
+	public void rendreAlive(){
+		alive = true;
+	}
+	
+	public void rendreCouler(){
+		alive = false;
+	}
+	
 	
 	public boolean tir(Plateau plateauJoueur, int x, int y){
 		
